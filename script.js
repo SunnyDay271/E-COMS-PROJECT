f45e1b0dfb54e6f89a7461a046f1bf4ae3de6c6c
SunnyDay271 <thinman27l@gmail.com>
2024-11-21 08:39:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// toggle class active
+const navbarNav = document.querySelector ('.Navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('.hamburger-menu').
+onclick = ( ) => {
+    navbarNav.classList.toggle('active')
+}
